9e24a6d901481216b6c70645f2ef8fd4f4deb1b4
uniyunnnn <kacl2261@gmail.com>
2023-05-22 14:29:09 +0900

    [feat] SecurityConfig생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당파일로 시큐리티 활성화 
+@Configuration // IoC컨테이너로 넣어주는 어노테이션
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super.configure(http); // super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
+		
+		http.authorizeRequests() // 이 주소경로로 요청이 들어오면
+        .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**")
+        .authenticated() // 인증이 필요하다.
+        .anyRequest() // 그 외의 요청들은
+        .permitAll() // 모두 허용한다.
+        .and() // 그리고
+        .formLogin() // 로그인(인증)이 필요한 요청이 들어오면
+        .loginPage("/auth/signin") // 로그인페이지 auth/signin 으로 이동시키고
+        .defaultSuccessUrl("/"); // 인증이 정상적으로 완료되면 / 로 이동한다.
+
+	}
+}
