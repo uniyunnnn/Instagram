5fcccca46b96c8d34f4c6590019d18ce75558fc9
ljh88820 <ljh88820@naver.com>
2023-06-02 12:00:45 +0900

    <feat> 비밀번호 해시 암호화, 유저 권한 부여 기능 추가

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.cos.photogramstart.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // 해당파일로 시큐리티 활성화 
 @Configuration // IoC컨테이너로 넣어주는 어노테이션
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.cos.photogramstart.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.cos.photogramstart.domain.user.Users;
 import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.domain.user.Users;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +14,15 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCrpBCryptPasswordEncoder;
 	
+	@Transactional //Write(insert,Update,Delete)
 	public Users 회원가입 (Users user) {
-		//회
+		//회원가입 진행
+		String rawPassword = user.getPassword();
+		String encPassword = bCrpBCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setRole("ROLE_USER");//관리자 ROLE_ADMIN
 		Users userEntity = userRepository.save(user);
 		return userEntity;
 	}
