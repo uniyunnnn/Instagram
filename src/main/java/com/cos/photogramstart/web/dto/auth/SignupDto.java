729578aa5068a9cf943b4317c7d3367b1391473e
ljh88820 <ljh88820@naver.com>
2023-06-02 12:47:16 +0900

    <feat> 회원가입 AOP 전처리 , 후처리 기능 추가

--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,14 +1,22 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import com.cos.photogramstart.domain.user.Users;
 
 import lombok.Data;
 
 @Data 
 public class SignupDto { //post으로 담아서 보낼려고 SingupDto를 사용
+	
+	@Max(20)
     private String username;
+	@NotBlank
     private String password;
+	@NotBlank
     private String email;
+	@NotBlank
     private String name;
     
     public Users toEntity() {
