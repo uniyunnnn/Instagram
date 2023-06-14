62c1641818e4dba68da4a26b37869fcc7298d1af
uniyunnnn <kacl2261@gmail.com>
2023-06-14 15:45:11 +0900

    <feat> 회원가입 - 유효성검사 @ResponseBody 사용 및 글로벌예외 처리하기

--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,7 +1,7 @@
 package com.cos.photogramstart.web.dto.auth;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import com.cos.photogramstart.domain.user.Users;
 
@@ -10,7 +10,9 @@ import lombok.Data;
 @Data 
 public class SignupDto { //post으로 담아서 보낼려고 SingupDto를 사용
 	
-	@Max(20)
+//	@Max(20)// 20 이하인가? 해서 맞으면 돌아가야하는데 안돌아감. 
+	@Size(min=2, max= 20) 
+	@NotBlank
     private String username;
 	@NotBlank
     private String password;
