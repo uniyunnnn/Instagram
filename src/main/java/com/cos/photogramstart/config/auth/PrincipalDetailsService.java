170b726a7236db6ee4b786c929543cab85cfe56a
BackJW <jnwoo97@gmail.com>
2023-06-15 15:21:23 +0900

    [feat] DetailsService 완성하고 로그인 세션 저장

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -5,13 +5,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service //IoC 등록
+import com.cos.photogramstart.domain.user.Users;
+import com.cos.photogramstart.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service // IoC 등록
 public class PrincipalDetailsService implements UserDetailsService {
 
-    
-    @Override
+	private final UserRepository userRepository;
+
+	// 패스워드는 알아서 체킹하니까 신경쓸 필요 X
+	// 리턴이 잘되면 자동으로 세션을 만든다.
+	@Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    	System.out.println("나실행돼?");
-    	return null;
-    }
+    	
+    	Users userEntity = userRepository.findByUsername(username);
+    	
+    	if(userEntity==null) {
+    		return null;
+    	}else {
+    		return new PrincipalDetails(userEntity);
+    		}
+    	}
 }
