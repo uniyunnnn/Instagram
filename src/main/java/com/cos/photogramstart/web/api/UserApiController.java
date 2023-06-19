2f82ba77c679d94ebb08393bfc9e9ede1ff425d0
uniyunnnn <kacl2261@gmail.com>
2023-06-19 18:21:00 +0900

    <fix> 회원정보수정- 누락된 UserApiController 추가

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,16 +1,30 @@
 package com.cos.photogramstart.web.api;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.user.Users;
+import com.cos.photogramstart.service.UserService;
+import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.web.dto.user.UserUpdateDto;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cos.photogramstart.web.dto.user.UserUpdateDto;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RestController
 public class UserApiController {
+
+    private final UserService userService;
     
     @PutMapping("/api/user/{id}")
-    public String update(UserUpdateDto userUpdateDto) {
-        System.out.println(userUpdateDto);
-        return "update 메서드 실행ok";
+    public CMRespDto<?> update(@PathVariable Integer id,
+    UserUpdateDto userUpdateDto,
+    @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Users userEntity = userService.회원수정(id, userUpdateDto.toEntity());
+        principalDetails.setUser(userEntity); //세선정보변경
+        return new CMRespDto<>(1, "회원수정완료", userEntity);
     }
 }
