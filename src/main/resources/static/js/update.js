2880ac49b6aedaa2f8184ec68bda18c7a8f95506
uniyunnnn <kacl2261@gmail.com>
2023-06-19 17:08:00 +0900

    <fix> 오타수정

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -9,7 +9,7 @@ function update(userId) {
         type: "put",
         url: `/api/user/${userId}`,
         data: data,
-        contentType: "application/x-www-form-urlencoded: charset=utf-8",
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType: "json"
     }).done(res => {
         console.log("update 성공", res)
