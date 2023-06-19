d1b93650be846f79cbe25c822d0b48e351aeb105
uniyunnnn <kacl2261@gmail.com>
2023-06-19 16:24:16 +0900

    Merge branch 'develop' of https://github.com/uniyunnnn/Instagram.git into develop

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,19 @@
 // (1) 회원정보 수정
-function update() {
+function update(userId) {
+	//form태그가 가지고 있는 값을 다담아서 넘긴다.
+    let data = $("#profileUpdate").serialize();
+    
+    console.log(data);
 
+    $.ajax({
+        type: "put",
+        url: `/api/user/${userId}`,
+        data: data,
+        contentType: "application/x-www-form-urlencoded: charset=utf-8",
+        dataType: "json"
+    }).done(res => {
+        console.log("update 성공", res)
+    }).fail(error => {
+        console.log("update 실패", error)
+    });
 }
