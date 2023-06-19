74a320fc54d8adcefaf4014a7eb49d841c8c874b
uniyunnnn <kacl2261@gmail.com>
2023-06-19 18:21:00 +0900

    <fix> 회원정보수정- 누락된 UserApiController 추가

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,12 +13,7 @@ import com.cos.photogramstart.config.auth.PrincipalDetails;
 public class UserController {
     
 	@GetMapping("/user/{id}")
-	public String profile(@PathVariable String id) {
-		try {
-			Integer integerId = Integer.parseInt(id);
-		} catch (NumberFormatException e) {
-		
-		}
+	public String profile(@PathVariable Integer id) {
 		return "user/profile";
     }
 	
