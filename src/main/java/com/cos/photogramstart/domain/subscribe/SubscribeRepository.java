b755448121394cd4c26a74e5943f0652c4e3db24
ljh88820 <ljh88820@naver.com>
2023-06-20 18:04:20 +0900

    <Bugfix> 구독,구독취소 API 만들기 버그 수정

--- src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -8,11 +8,11 @@ import org.springframework.data.repository.query.Param;
 public interface SubscribeRepository extends JpaRepository<Subscribe, Integer>{
 
 	@Modifying//INSERT,DELETE,UPDATE를 네이티브 쿼리로 작성하려면 해당 어노테이션 필요
-	 @Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
+	 @Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(fromUserId, toUserId, now())", nativeQuery = true)
 	void mSubscribe(@Param("fromUserId") Integer fromUserId, @Param("toUserId") Integer toUserId);
 
 	
 	@Modifying
-	 @Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
+	 @Query(value = "DELETE FROM subscribe WHERE fromUserId = fromUserId AND toUserId = toUserId", nativeQuery = true)
 	void mUnSubscribe(@Param("fromUserId") Integer fromUserId, @Param("toUserId") Integer toUserId);
 }
