623466ef70d4a7e85539d1503fcaaad1ac95a345
ljh88820 <ljh88820@naver.com>
2023-06-21 15:35:56 +0900

    <feat> 구독하기 글러벌 예외처리하기, API주소 시큐리티 설정하기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.handler.ex.CustomApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
@@ -30,10 +31,20 @@ public class ControllerExceptionHandler {//모든 Exception을 여기로 받아�
 	
     // CMRespDto 오브젝트를 응답하는 핸들러
     @ExceptionHandler(CustomValidationApiException.class)
-    public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationApiException e) {
+    public ResponseEntity<?> validationApiException(CustomValidationApiException e) {
 //        log.info("==============================");
     	return new ResponseEntity<>(
                 new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()),
+                HttpStatus.BAD_REQUEST
+        );
+    }// 데이터를 리턴 
+    
+    // CMRespDto 오브젝트를 응답하는 핸들러
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> apiException(CustomValidationApiException e) {
+//        log.info("==============================");
+    	return new ResponseEntity<>(
+                new CMRespDto<>(-1, e.getMessage(),null),
                 HttpStatus.BAD_REQUEST
         );
     }// 데이터를 리턴 
--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +16,16 @@ public class SubscribeService {
 	
 	@Transactional
 	  public void 구독하기(Integer fromUserId, Integer toUserId) {
-		  subscribeRepository.mSubscribe(fromUserId, toUserId);
-		  
+		  try {
+			  subscribeRepository.mSubscribe(fromUserId, toUserId);
+		  }catch(Exception e) {
+			  throw new CustomApiException("이미 구독을 하였습니다.");
+		  }
 	  }
 	
 	@Transactional
      public void 구독취소하기(Integer fromUserId, Integer toUserId) {
+		
 		 subscribeRepository.mUnSubscribe(fromUserId, toUserId);
 		  
 
