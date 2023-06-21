cc32facc84c6401dedab17949faf9aa5d5eb4e99
uniyunnnn <kacl2261@gmail.com>
2023-06-21 16:59:12 +0900

    <fix> image 서버에 업로드하기(UUID적용)

--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,10 +1,21 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.service.ImageService;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class ImageController {
+	
+	private final ImageService imageService;
 
 	@GetMapping({"/","/image/story"})
 	public String story() {
@@ -20,5 +31,12 @@ public class ImageController {
 	public String upload() {
 		return "image/upload";
 	}
+
+	@PostMapping("/image")
+    public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        //서비스호출
+		imageService.사진업로드(imageUploadDto, principalDetails);
+		return "redirect:/user/" + principalDetails.getUser().getId();
+    }
 
 }
