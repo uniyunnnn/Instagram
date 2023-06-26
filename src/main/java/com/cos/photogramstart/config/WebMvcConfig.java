279e9e2eb89d55d58d68fe7e7ab616ae978e4d58
uniyunnnn <kacl2261@gmail.com>
2023-06-26 16:04:31 +0900

    <fix> Image 뷰 렌더링

---
+++ src/main/java/com/cos/photogramstart/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer { // web 설정 파일 
+	
+	@Value("${file.path}")
+    private String uploadFolder;
+
+	@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			WebMvcConfigurer.super.addResourceHandlers(registry);
+			
+			registry
+				.addResourceHandler("/upload/**") //jsp 페이지에서 /upload/** 주소 패턴이 나오면 발동
+				.addResourceLocations("file:///"+uploadFolder)
+				.setCachePeriod(60*10*6) //1시간동안 이미지캐싱
+				.resourceChain(true)
+				.addResolver(new PathResourceResolver());
+	}
+}
