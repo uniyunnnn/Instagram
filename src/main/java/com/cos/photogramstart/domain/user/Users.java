e2784a3b65e77be0892095fbedbb8ebb5b4b1a37
BackJW <jnwoo97@gmail.com>
2023-06-26 15:09:00 +0900

    <feat> image 양방향 매핑 이해하기

--- src/main/java/com/cos/photogramstart/domain/user/Users.java
+++ src/main/java/com/cos/photogramstart/domain/user/Users.java
@@ -1,13 +1,18 @@
 package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,35 +28,43 @@ public class Users {
 
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 DB설정값(MySQL/Oracle)으로 따라간다.
 	@Id // Primary Key를 지정해주는 어노테이션
-    private Integer id; // 데이터가 들어갈 때 마다 번호를 매겨줄것임.
+	private Integer id; // 데이터가 들어갈 때 마다 번호를 매겨줄것임.
 
-	@Column(length = 20 ,unique=true)
-    private String username; // 아이디
-    @Column(nullable = false)
-    private String password; // 비밀번호
-    @Column(nullable = false)
-    private String name; // 별명
+	@Column(length = 20, unique = true)
+	private String username; // 아이디
+	@Column(nullable = false)
+	private String password; // 비밀번호
+	@Column(nullable = false)
+	private String name; // 별명
 
-    private String website; // 개인 웹사이트 주소
+	private String website; // 개인 웹사이트 주소
 
-    private String bio; // 자기소개
-    
-    @Column(nullable = false)
-    private String email; // 이메일
+	private String bio; // 자기소개
 
-    private String phone; // 전화번호
+	@Column(nullable = false)
+	private String email; // 이메일
 
-    private String gender; // 성별
+	private String phone; // 전화번호
 
-    private String profileImageUrl; // 프로필 사진
+	private String gender; // 성별
 
-    private String role; // 권한
+	private String profileImageUrl; // 프로필 사진
 
-    private LocalDateTime createDate; // 데이터가 입력된 시간.
+	private String role; // 권한
 
-    @PrePersist
-    public void createDate() {
-        this.createDate = LocalDateTime.now();
-    }
+	// 나는 연관관계의 주인이 아니다. 그러므로 테이블에 칼럼을 만들지마라
+	// User를 Select할 때 해당 User id로 등록된 image들을 다 가져와
+	// Lazy 일때는 User를 Selcet할때 해당 User id로 등록된 Image들을 가져오지마!! -> 대신 getImages() 함수가
+	// 호출 될때~
+	// Eager = User를 Select할 때 해당 User id로 등록된 image들을 전부 조인해서 가져와
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Image> images;
+
+	private LocalDateTime createDate; // 데이터가 입력된 시간.
+
+	@PrePersist
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
 
 }
