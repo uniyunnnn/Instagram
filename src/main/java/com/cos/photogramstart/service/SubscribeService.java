f1bf36d6b806687579f30dc7892bc6867c7e6850
uniyunnnn <kacl2261@gmail.com>
2023-07-03 15:41:06 +0900

    <feat> 구독 모달 정보를 위한 API 만들기

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,10 +1,13 @@
 package com.cos.photogramstart.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +16,12 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 
 	private final SubscribeRepository subscribeRepository;
+	
+	@Transactional(readOnly = true)
+	public List<SubscribeDto> 구독리스트(Integer principalId,Integer pageUserId){
+		
+		return null;
+	};
 	
 	@Transactional
 	  public void 구독하기(Integer fromUserId, Integer toUserId) {
