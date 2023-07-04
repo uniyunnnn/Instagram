cd9c7a7664fc62dbc5b119b845489db6ab4bc4b4
BackJW <jnwoo97@gmail.com>
2023-07-04 15:12:49 +0900

    <feat> 포토리스트 API 만들기

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -1,7 +1,11 @@
 package com.cos.photogramstart.domain.image;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image, Integer>{
-    
+	@Query(value = "SELECT * FROM image WHERE userId IN(SELECT toUserId FROM subscribe WHERE fromUserId = 2)", nativeQuery = true)
+	List<Image> mStory(Integer principalId);
 }
