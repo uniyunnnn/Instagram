1882dcede9f48ef923244767c8e929c01bb488dd
uniyunnnn <kacl2261@gmail.com>
2023-07-07 16:07:04 +0900

    <feat> 인기 페이지 구현

--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,11 +1,15 @@
 package com.cos.photogramstart.web;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.ImageService;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
@@ -24,7 +28,9 @@ public class ImageController {
 	}
 
 	@GetMapping("/image/popular")
-	public String popular() {
+	public String popular(Model model) {
+		List<Image> images = imageService.인기사진();
+	    model.addAttribute("images", images);
 		return "image/popular";
 	}
 
