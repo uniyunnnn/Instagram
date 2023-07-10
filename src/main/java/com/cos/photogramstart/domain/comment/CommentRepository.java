2b380b3b76d189a229357453a331839bc10605e7
ljh88820 <ljh88820@naver.com>
2023-07-10 16:52:45 +0900

    <feat> 댓글 - 댓글쓰기 Ajax 함수 만들기

--- src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
+++ src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.cos.photogramstart.domain.comment;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CommentRepository extends JpaRepository<Comments, Integer>{
+	
+	@Modifying
+    @Query(value = "INSERT INTO comments(content, imageId, userId, createDate) VALUES(:content, :imageId, :userId,SYSDATE)", nativeQuery = true)
+    Comments save(String content, Integer imageId, Integer userId);
 }
--- src/main/java/com/cos/photogramstart/service/CommentService.java
+++ src/main/java/com/cos/photogramstart/service/CommentService.java
@@ -1,10 +1,15 @@
 package com.cos.photogramstart.service;
 
-import com.cos.photogramstart.domain.comment.Comment;
-import com.cos.photogramstart.domain.comment.CommentRepository;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.photogramstart.domain.comment.CommentRepository;
+import com.cos.photogramstart.domain.comment.Comments;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.domain.user.Users;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +18,27 @@ import lombok.RequiredArgsConstructor;
 public class CommentService {
 
     private final CommentRepository commentRepository;
-
+    private final UserRepository userRepository;
+    
     @Transactional
-    public Comment 댓글쓰기() {
-    	return null;
+    public Comments 댓글쓰기(String content, Integer imageId, Integer userId) {
+    	
+    	//Tip(객체를 만들 때 id값만 담아서 insert할 수 있다. )
+    	//대신 return시에 image객체와 user객체는 id값만 가지고 잇는 빈 객체를 리턴받는다.
+    	Image image = new Image();
+        image.setId(imageId);
+        
+        Users userEntity = userRepository.findById(userId).orElseThrow(() -> {
+            throw new CustomApiException("유저 아이디를 찾을 수 없습니다.");
+        });
+    	
+    	
+    	Comments comment = new Comments();
+    	comment.setContent(content);
+    	comment.setImage(image);
+    	comment.setUser(userEntity);
+    	
+    	return commentRepository.save(comment);
     }
 
     @Transactional
