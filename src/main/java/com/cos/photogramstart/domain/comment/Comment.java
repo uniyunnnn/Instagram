d5d4e2d56ba02aa0991e9896c0b797e6fb6bca04
uniyunnnn <kacl2261@gmail.com>
2023-07-10 15:01:20 +0900

    <feat> Comment 모델 만들기

---
+++ src/main/java/com/cos/photogramstart/domain/comment/Comment.java
@@ -0,0 +1,51 @@
+package com.cos.photogramstart.domain.comment;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.Users;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    @JoinColumn(name = "userId")
+    @ManyToOne(fetch = FetchType.EAGER) 
+    private Users user;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne(fetch = FetchType.EAGER) 
+    private Image image;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
